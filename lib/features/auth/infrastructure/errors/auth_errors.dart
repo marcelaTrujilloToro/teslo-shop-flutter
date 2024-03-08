69206b8b49f847cba5135b1de89bdab411a19008
Marcela Trujillo Toro <marces.ttblue@gmail.com>
2024-03-08 12:14:24 -0500

    feat: error handler, providers, authentication with jwt

---
+++ lib/features/auth/infrastructure/errors/auth_errors.dart
@@ -0,0 +1,15 @@
+class WrongCredentias implements Exception {}
+
+class InvalidToken implements Exception {}
+
+class ConnectionTimeout implements Exception {}
+
+class CustomError implements Exception {
+  final String message;
+  final bool loggedRequired;
+  // final int errorCode;
+
+  CustomError(this.message, [this.loggedRequired = false]
+      // this.errorCode,
+      );
+}
---
+++ lib/features/auth/infrastructure/infrastructure.dart
@@ -0,0 +1,7 @@
+export 'datasourses/auth_datasource_impl.dart';
+
+export 'repositories/auth_repository_impl.dart';
+
+export 'mappers/user_mapper.dart';
+
+export 'errors/auth_errors.dart';
